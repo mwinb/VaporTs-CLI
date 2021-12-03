e331395305d64c837b811731153fffe5b16b89f5
mwinb <mwinberry0101@gmail.com>
2021-12-03 08:39:57 -0800

    Fix prepublish command to use unit test to prevent hanging on release

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 #! /usr/bin/env node
-
+import * as yargs from 'yargs';
 import { log } from './common/utils';
+
+// const usage = '\nUsage: tran <lang_name> sentence to be translated';
+// const options = yargs
+//   .usage(usage)
+//   .option('l', { alias: 'languages', describe: 'List all supported languages.', type: 'boolean', demandOption: false })
+//   .help(true).argv;
 
 export function main() {
   log('VaporTs CLI');
