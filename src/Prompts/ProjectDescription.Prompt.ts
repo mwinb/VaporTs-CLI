a6f0579a406a8f7ca46e677974ee65b59e0673f3
mwinb <mwinberry0101@gmail.com>
2021-12-06 10:53:18 -0800

    #4. Added project description prompt

---
+++ src/Prompts/ProjectDescription.Prompt.ts
@@ -0,0 +1,9 @@
+import { PromptObject } from 'prompts';
+
+const projectDescriptionPrompt: PromptObject = {
+  type: 'text',
+  name: 'projectDescription',
+  message: 'Enter project description'
+};
+
+export default projectDescriptionPrompt;
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@
 
 import PromptReader from './Classes/PromptReader';
 import authorPrompt from './Prompts/Author.Prompt';
+import projectDescriptionPrompt from './Prompts/ProjectDescription.Prompt';
 import projectNamePrompt from './Prompts/ProjectName.Prompt';
 import versionPrompt from './Prompts/Version.Prompt';
 
 export async function main() {
   console.log('VaporTs CLI');
-  const result = await PromptReader.readPrompts([projectNamePrompt, authorPrompt, versionPrompt]);
+  const result = await PromptReader.readPrompts([
+    projectNamePrompt,
+    authorPrompt,
+    versionPrompt,
+    projectDescriptionPrompt
+  ]);
   console.log(JSON.stringify(result, null, 2));
 }
 
