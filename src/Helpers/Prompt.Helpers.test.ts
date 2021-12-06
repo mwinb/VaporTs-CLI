507a4bf77df2c308c0cef39580f0b531067d7b4b
mwinb <mwinberry0101@gmail.com>
2021-12-06 10:38:10 -0800

    #3. Add version prompt to request project version number. Add versionValidator that uses semver validate to ensure a valid version number is provided.

--- src/Helpers/Prompt.Helpers.test.ts
+++ src/Helpers/Prompt.Helpers.test.ts
@@ -1,4 +1,5 @@
-import { getUserCurrentFolder, projectNameValidator } from './Prompt.Helpers';
+import { getUserCurrentFolder, projectNameValidator, versionValidator } from './Prompt.Helpers';
+import * as semver from 'semver';
 
 describe('Getting user cwd', () => {
   beforeEach(() => {
@@ -14,5 +15,21 @@ describe('project name validator (regex taken from npm package.json docs)', () =
   it('enforces the npm project name regex.', () => {
     expect(projectNameValidator('some invalid name')).toBeFalsy();
     expect(projectNameValidator('@valid/project.name')).toBeTruthy();
+  });
+});
+
+describe('semver validation', () => {
+  it('validates version input using semver validate', () => {
+    jest.spyOn(semver, 'valid');
+    versionValidator('v1.0.0');
+    expect(semver.valid).toHaveBeenCalledWith('v1.0.0');
+  });
+
+  it('returns true given a valid semver version', () => {
+    expect(versionValidator('v1.0.0')).toBeTruthy();
+  });
+
+  it('returns false given an invalid version', () => {
+    expect(versionValidator('a.b.c')).toBeFalsy();
   });
 });
